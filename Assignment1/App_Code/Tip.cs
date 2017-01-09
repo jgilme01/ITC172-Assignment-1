72fbe6890e0a9e3f04e3c84728e961a9b1941d78
jgilme01 <jgilme01@seattlecentral.edu>
2017-01-09 12:15:35 -0800

    Add files via upload

---
+++ Assignment1/App_Code/Tip.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Tip
+/// </summary>
+public class Tip
+{
+    //fields are class level variables that describe the class
+    public double MealAmount { set; get; }
+    public double TipPercent { set; get; }
+    private const double TAXPERCENT = .09;
+    public Tip()
+    {
+        MealAmount = 0;
+        TipPercent = 0;
+    }
+    //These are double constructors, two ways to start
+    public Tip(double meal, double percent)
+    {
+        MealAmount = meal;
+        TipPercent = percent;
+        //could also use this keyword. Example this.MealAmount = meal
+    }
+    public double CalculateTax()
+    {
+        return MealAmount * TAXPERCENT;
+    }
+
+    public double CalculateTip()
+    {
+        return MealAmount * TipPercent;
+    }
+
+    public double CalculateTotal()
+    {
+        return MealAmount + CalculateTax() + CalculateTip();
+    }
+}
---
+++ Assignment1/Default.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>tip calculator</title>
+</head>
+<body>
+    <form id="form1" runat="server"><!--all has to exist in form-->
+    <div>
+        <p>
+        <asp:Label ID="Label1" runat="server" Text="Enter Meal Amount"></asp:Label>
+        <asp:TextBox ID="MealTextBox" runat="server"></asp:TextBox><!--renamed TextBox to refer in code-->
+        <asp:RadioButtonList ID="TipPercentsRadioButtonList" runat="server"></asp:RadioButtonList>
+        <asp:TextBox ID="OtherTextBox" runat="server"></asp:TextBox>
+        <p><asp:Button ID="SubmitButton" runat="server" Text="Button" OnClick="SubmitButton_Click" /></p>
+        <asp:Label ID="ResultLabel" runat="server" Text="Label"></asp:Label>
+        </p>
+    </div>
+    </form>
+</body>
+</html>
